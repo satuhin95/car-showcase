56019c6aa420de5c7cb1311fd77829496cb8f558
satuhin95 <saifcm95@gmail.com>
2023-06-10 17:08:55 +0600

    card design

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,16 +2,27 @@
 import { CustomButtonProps } from "@/types";
 import Image from "next/image"
 
-const CustomButton = ({title, containerStyles,handleClick, btnType}:CustomButtonProps) => {
+const CustomButton = ({title, containerStyles,handleClick, btnType,rightIcon,textStyles,isDisabled}:CustomButtonProps) => {
   return (
-    <button disabled={false}
+    <button
+     disabled={false}
     type={btnType || "button"}
     className={`custom-btn ${containerStyles}`}
     onClick={handleClick}
     >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
+        {rightIcon && (
+          <div className="relative w-6 h-6">
+            <Image 
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+            />
+          </div>
+        )}
     </button>
   )
 }
