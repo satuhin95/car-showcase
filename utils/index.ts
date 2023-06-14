83e549998195638af7896e813398d26d78d3b311
satuhin95 <saifcm95@gmail.com>
2023-06-14 11:23:19 +0600

    fatch params update

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,12 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(){
+export async function fetchCars(filters:FilterProps){
+  const {manufacturer,year,model,limit,fuel} = filters;
     const headers= {
 		'X-RapidAPI-Key': '7c3e44048bmshf4d04d3c4715fedp14eeb6jsn5a5c180a5734',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',{
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{
         headers:headers,
     });
     const result = await response.json();
